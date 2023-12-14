d97f509f4469955ce67defbd5cf0910e7bfacf76
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-12-14 13:39:32 +0500

    admin controller yozildi

--- server/src/modules/admin/_controller.js
+++ server/src/modules/admin/_controller.js
@@ -1,7 +1,22 @@
-export const listAdmin = async (req, res) => {
+import { LoginSchema } from '../../utils/joi.js'
+import { addAdminService } from './Service/addAdmin.js'
+import { listAdminService } from './Service/listAdmin.js'
+
+export const addAdmin = async (req, res, next) => {
+  try {
+    // const validation = LoginSchema.validate(req.body.email, req.body.password)
+
+    const result = await addAdminService({ body: req.body })
+    res.status(201).json({ data: result })
+  } catch (error) {
+    next(error)
+  }
+}
+export const listAdmin = async (req, res, next) => {
   try {
     const result = await listAdminService()
+    res.status(200).json({ data: result })
   } catch (error) {
-    console.log(error)
+    next(error)
   }
 }
