d347e8c693880dcf72dbc0230ce0cd4bb764c84c
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-12-16 18:48:43 +0500

    Joi validator configuration yozildi

--- server/src/utils/joi.js
+++ server/src/utils/joi.js
@@ -1,14 +1,17 @@
 import Joi from 'joi'
 
-export const LoginSchema = Joi.object({
+export const validateLogin = Joi.object({
   email: Joi.string().required(),
   password: Joi.string().required(),
 })
+export const validateAddUser= Joi.object({
+  login: Joi.string().required(),
+  password: Joi.string().required(),
+})
 
-// export const RegisterSchema = Joi.object({
-//   username: Joi.string().max(32).required(),
-//   password: Joi.string().required(),
-//   repeat_password: Joi.ref('password'),
-//   email: Joi.string().required(),
-//   phone: Joi.string().required(),
-// })
+export const validateProduct = Joi.object({
+  name: Joi.string().required(),
+  manufacturer: Joi.string().required(),
+  brand: Joi.string().required(),
+  quantity: Joi.number().required(),
+})
