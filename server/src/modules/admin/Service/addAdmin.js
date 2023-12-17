449596871e18d930d9e631a65286361778f38749
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-12-17 14:13:53 +0500

    addAdmin service yozildi

--- server/src/modules/admin/Service/addAdmin.js
+++ server/src/modules/admin/Service/addAdmin.js
@@ -2,16 +2,22 @@ import { BadRequestError } from '../../../utils/errors/index.js'
 import { Admin } from '../Schema/Admin.js'
 import bcrypt from 'bcrypt'
 
-export const addAdminService = async ({ body }) => {
+export const addAdminService = async ({ body, user }) => {
   const { email, password, full_name } = body
-  console.log(body)
+  const { id } = user
 
+  const checking = await Admin.findOne({ _id: id })
+
+  if (!checking) {
+    throw new BadRequestError('Sizda adminlik huquqi yo`q')
+  }
+  
   const admin = await Admin.findOne({ email })
 
   if (admin) {
     throw new BadRequestError('Bu admin oldin ro`yxatdan o`tgan')
   }
-  const hashedPass =  bcrypt.hashSync(password,10)
+  const hashedPass = bcrypt.hashSync(password, 10)
 
   const newAdmin = await Admin.create({
     full_name,
