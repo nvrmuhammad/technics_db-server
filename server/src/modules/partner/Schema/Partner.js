7f0a00475ddaf5269f75b455dca0f5fee57bb1ec
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-12-19 21:13:35 +0500

    Partner schema tuzildi

---
+++ server/src/modules/partner/Schema/Partner.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const PartnerSchema = new mongoose.Schema(
+  {
+    full_name: {
+      type: mongoose.SchemaTypes.String,
+      required: true,
+    },
+    admin_id: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'admin',
+      require: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
+    versionKey: false,
+  }
+)
+
+export const Partner = mongoose.model('partner', PartnerSchema)
