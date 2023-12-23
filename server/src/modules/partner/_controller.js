1b439caf3cab04eca2943c733d18914bfe3b0f67
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-12-23 11:51:41 +0500

    Partner controller yozildi

--- server/src/modules/partner/_controller.js
+++ server/src/modules/partner/_controller.js
@@ -1,3 +1,11 @@
+import { BadRequestError } from '../../utils/errors/index.js'
+import { validatedPartner } from '../../utils/joi.js'
+import { addPartnerService } from './Service/addPartner.js'
+import { listPartnerService } from './Service/listPartner.js'
+import { oncePartnerService } from './Service/oncePartner.js'
+import { removePartnerService } from './Service/removePartner.js'
+import { updatePartnerService } from './Service/updatePartner.js'
+
 export const addPartner = async (req, res, next) => {
   try {
     const { error } = validatedPartner.validate(req.body)
@@ -19,6 +27,7 @@ export const listPartner = async (req, res, next) => {
     next(error)
   }
 }
+
 export const oncePartner = async (req, res, next) => {
   try {
     const result = await oncePartnerService({
