e8da6d3c1dd5d65248ebee8d2ba760ee04a0766b
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-12-25 14:02:58 +0500

    loginUser controller yozildi

--- server/src/modules/user/_controller.js
+++ server/src/modules/user/_controller.js
@@ -14,3 +14,19 @@ export const addUser = async (req, res, next) => {
     next(error)
   }
 }
+export const loginUser = async (req, res, next) => {
+  try {
+    const { error } = validatedUserLogin.validate(req.body)
+    if (error) {
+      throw new BadRequestError('Ma`lumotni to`liq kiriting')
+    }
+    const result = await loginUserService({
+      body: req.body,
+      user: req.user,
+    })
+
+    res.status(200).json({ data: result })
+  } catch (error) {
+    next(error)
+  }
+}
